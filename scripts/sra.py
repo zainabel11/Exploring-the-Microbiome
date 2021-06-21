a31841e9b9789408e7017182396b38c505d9b7f1
SofSei <sofiasehli00@gmail.com>
2021-06-21 15:45:47 +0100

    correct

--- scripts/sra.py
+++ scripts/sra.py
@@ -2,4 +2,3 @@ import subprocess
 def retrive(id, output):
     with open (f"{output}/retrive.out", 'w') as out, open(f'{output}/retrive.err','w') as err:
         return subprocess.call(f'SRAdownload -f {output} {id}', shell=True, stdout=out,stderr=err)
-retrive("SRR2223576", '/home/abdellah')
